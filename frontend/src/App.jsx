f055af46b3276ec641b40ceaf26370a61d262808
Mohammad Saleh <mohammad.saleh.3004@gmail.com>
2025-10-16 20:18:56 +0300

    commited workflows ci

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
+  //let name="mhmd";
   return (
     <div className="relative h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
